f33efab61206869d50e08acf42974749a7d95d9a
waxew <4622.ar@gmail.com>
2026-08-23 15:13:23 +0330

    Route booking staff capacity manager

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -7,6 +7,7 @@ import InstagramControlCenter from './InstagramControlCenter';
 import BookingManagerV2 from './BookingManagerV2';
 import BookingBusinessTools from './BookingBusinessTools';
 import BookingAutomations from './BookingAutomations';
+import BookingStaffManager from './BookingStaffManager';
 import PublicBookingPage from './PublicBookingPage';
 import CommerceQuickNav from './CommerceQuickNav';
 import './styles.css';
@@ -23,11 +24,13 @@ const rootView = path.startsWith('/book/')
         ? <InstagramControlCenter />
         : path === '/app/booking/automations'
           ? <BookingAutomations />
-          : path === '/app/booking/tools'
-            ? <BookingBusinessTools />
-            : path === '/app/booking'
-              ? <BookingManagerV2 />
-              : <App />;
+          : path === '/app/booking/staff'
+            ? <BookingStaffManager />
+            : path === '/app/booking/tools'
+              ? <BookingBusinessTools />
+              : path === '/app/booking'
+                ? <BookingManagerV2 />
+                : <App />;
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
