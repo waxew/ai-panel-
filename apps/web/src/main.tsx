9125ba39214eb3c5dfce3bc00c3352b68bcbd810
waxew <4622.ar@gmail.com>
2026-08-23 15:02:43 +0330

    Add booking automation route

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -6,6 +6,7 @@ import StoreOrders from './StoreOrders';
 import InstagramControlCenter from './InstagramControlCenter';
 import BookingManagerV2 from './BookingManagerV2';
 import BookingBusinessTools from './BookingBusinessTools';
+import BookingAutomations from './BookingAutomations';
 import CommerceQuickNav from './CommerceQuickNav';
 import './styles.css';
 
@@ -15,11 +16,13 @@ const rootView = window.location.pathname === '/app/telegram-builder'
     ? <StoreOrders />
     : window.location.pathname === '/app/instagram'
       ? <InstagramControlCenter />
-      : window.location.pathname === '/app/booking/tools'
-        ? <BookingBusinessTools />
-        : window.location.pathname === '/app/booking'
-          ? <BookingManagerV2 />
-          : <App />;
+      : window.location.pathname === '/app/booking/automations'
+        ? <BookingAutomations />
+        : window.location.pathname === '/app/booking/tools'
+          ? <BookingBusinessTools />
+          : window.location.pathname === '/app/booking'
+            ? <BookingManagerV2 />
+            : <App />;
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
