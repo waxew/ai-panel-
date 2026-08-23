659ec2c4c7b086e94138373d8908f7a77471c50a
waxew <4622.ar@gmail.com>
2026-08-23 14:58:36 +0330

    Route booking business tools hub

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -5,6 +5,7 @@ import TelegramMenuBuilder from './TelegramMenuBuilder';
 import StoreOrders from './StoreOrders';
 import InstagramControlCenter from './InstagramControlCenter';
 import BookingManagerV2 from './BookingManagerV2';
+import BookingBusinessTools from './BookingBusinessTools';
 import CommerceQuickNav from './CommerceQuickNav';
 import './styles.css';
 
@@ -14,9 +15,11 @@ const rootView = window.location.pathname === '/app/telegram-builder'
     ? <StoreOrders />
     : window.location.pathname === '/app/instagram'
       ? <InstagramControlCenter />
-      : window.location.pathname === '/app/booking'
-        ? <BookingManagerV2 />
-        : <App />;
+      : window.location.pathname === '/app/booking/tools'
+        ? <BookingBusinessTools />
+        : window.location.pathname === '/app/booking'
+          ? <BookingManagerV2 />
+          : <App />;
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
