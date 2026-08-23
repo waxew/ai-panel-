8aa4db82198e7e8350e1310a8e51eba45c1d1a25
waxew <4622.ar@gmail.com>
2026-08-23 16:47:34 +0330

    Sync production migration 20260823123239

---
+++ supabase/migrations/20260823123239_grant_channel_commerce_rpc.sql
@@ -0,0 +1,3 @@
+grant execute on function public.channel_cart_snapshot(text,text,text) to service_role;
+grant execute on function public.channel_cart_change(text,text,text,text,text,text,integer) to service_role;
+grant execute on function public.channel_checkout_cart(text,text,text,text,text) to service_role;
